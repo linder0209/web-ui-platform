195e5d38815f414500ef15f8adbcde0dd0725f54
linder0209 <Linder.Wang@Everbridge.com>
2013-01-24 18:43:48 +0800

    web ui platform

--- WebContent/web/jquery/myplugins/forms/javascript/hopefuture.ui.selector.js
+++ WebContent/web/jquery/myplugins/forms/javascript/hopefuture.ui.selector.js
@@ -28,6 +28,7 @@
             cls: '', //自定义class
             style: '', //自定义style
             mutable: true, //记录可变的
+            multiple: true,
             buttonIcon: '', //button 图标
             removeText: '',
             width: 200,
@@ -39,7 +40,7 @@
                 parentId: '',
                 text: '',
                 value:'',
-                leaf: true,//如果children有值，则leaf默认为false
+                leaf: true,//如果children有值，但leng属性不大于0 则leaf默认为true，即没有子元素就认为是叶子节点，符合客观规律
                 children:[]
               }]
              */
@@ -59,56 +60,28 @@
             .addClass('ui-widget ui-widget-content ui-corner-all');
             this.uiSelector = $(this.selectorUl).appendTo(this.uiSelectorCt);
             this.element.after(this.uiSelectorCt);
-            var me = this;
-
+            
+            if(this.options.multiple === false){
+                this._setSingle();
+                this.options.mutable = false;
+            }
+            
+            var data, me = this;
             if (element[0].tagName.toLowerCase() === 'select') {
-                element.find('optgroup, option').each(function() {
-                    if ($(this).is('optgroup')) {
-                        var optgroup = $(this),
-                             text = optgroup.attr('label'),
-                             value = optgroup.attr('value');
-                        me.lastOptgroup = me._addItemCt(value,text);
-                    } else {
-                        var value = $(this).val(),
-                            text = $(this).text();
-                        me._addItem(value, text, me.lastOptgroup);
-                    }
-                });
+                data = this._selectElToData(element);
+                this._createItem(data, 0);
             }else {//可利用传入的数据来设置
-                var data = this.options.data;
-                data = [{
-                        id: '1',
-                        text: 'item1',
-                        value: '1',
-                        children: [{
-                                id: '11',
-                                text: 'item11',
-                                value: '11',
-                                leaf: true
-                            },{
-                                id: '12',
-                                text: 'item12',
-                                value: '12',
-                                leaf: true
-                            }]
-                    },{
-                        id: '2',
-                        text: 'item1',
-                        value: '2',
-                        children: [{
-                                id: '21',
-                                text: 'item21',
-                                value: '21',
-                                leaf: true
-                            },{
-                                id: '22',
-                                text: 'item22',
-                                value: '22',
-                                leaf: true
-                            }]
-                    }];
+                data = this.options.data;
                 if(data){
-                    
+                    this._createItem(data, 0);
+                }else if (this.options.url){
+                    $.ajax(this.options.url,{
+                        success : function(resData){
+                            if(resData){
+                                me._createItem(data, 0);
+                            }
+                        }
+                    });
                 }
             }
             
@@ -117,20 +90,81 @@
                  this._remove(event);
                  return false;
             };
-            events['mouseenter .' + this.selectorCls.selectableCls] = function(event) {
+            events['mouseenter .' + this.selectorCls.contentCls] = function(event) {
                  var target = $(event.currentTarget);
-                 target.addClass('ui-state-focus');
+                 target.parent().addClass('ui-state-focus');
             };
-            events['mouseleave .' + this.selectorCls.selectableCls] = function(event) {
+            events['mouseleave .' + this.selectorCls.contentCls] = function(event) {
                  var target = $(event.currentTarget);
-                 target.removeClass('ui-state-focus');
+                 target.parent().removeClass('ui-state-focus');
             };
             
-            events['click .' + this.selectorCls.selectableCls] = function(event) {
+            events['click .' + this.selectorCls.contentCls] = function(event) {
                  var target = $(event.currentTarget);
                  //target.toggleClass('ui-state-active');
             };
             this._on(this.uiSelector, events);
+        },
+        
+        _setSingle : function(){
+            this.uiSelectorCt.css('min-height','0');
+            this.uiSelector.hide();
+            var single = $('<div class="' + this.options.baseCls + '-single" > \n\
+                <span></span><span class="' + this.options.baseCls + '-icon ui-icon ui-icon-carat-1-s"></span> </div>').prependTo(this.uiSelectorCt);
+            
+            var events = {};
+            events['click .' + this.options.baseCls + '-single'] = function(){
+                this.uiSelector.show();
+                single.addClass('ui-widget-content ui-corner-top');
+            };
+            events['click .' + this.selectorCls.contentCls] = function(event) {
+                 var target = $(event.currentTarget),
+                     text = target.children('span').text();
+                 single.removeClass('ui-widget-content ui-corner-top').children().eq(0).text(text);
+                 this.uiSelector.hide();
+                     
+            };
+            this._on(this.uiSelectorCt, events);
+            
+            this._on(this.document, {
+                click: function(event) {
+                    if (!$(event.target).closest(".h-ui-selector").length) {
+                        this.uiSelector.hide();
+                        single.removeClass('ui-widget-content ui-corner-top');
+                    }
+                }
+            });
+        },
+                
+        _selectElToData : function(element){
+            var data = [],subData,children;
+            element.find('optgroup, option').each(function() {
+                var item = $(this);
+                if (item.is('optgroup')) {
+                    children = [];
+                    subData = {
+                        id : item.attr('id') || item.attr('value'),
+                        value : item.attr('value'),
+                        text : item.attr('label'),
+                        leaf : false,
+                        children : children
+                    };
+                    data.push(subData);
+                }else{
+                    subData = {
+                        id : item.attr('id') || item.val(),
+                        value : item.val(),
+                        text : item.text(),
+                        leaf : true
+                    };
+                    if(children){
+                        children.push(subData);
+                    }else{
+                        data.push(subData);
+                    }
+                }
+            });
+            return data;
         },
                 
         _createElements: function() {
@@ -150,6 +184,7 @@
             this.selectorUl = '<ul class="' + cls.ulCls + '"></ul>';
             this.selectorLi = '<li class="' + cls.liCls + '" />';
             this.selectorContent = '<div class="' + cls.contentCls + '"><span>{text}</span>{removeBtn}</div>';
+            this.header = '<header class="' + cls.labelCls + '" />';
         },
         _destroy: function() {
             this.element.removeClass('ui-helper-hidden');
@@ -159,45 +194,130 @@
 
         },
         _remove: function(event) {
-            $(event.target).closest('li.' + this.selectorCls.liCls).remove();
-            var ui = $(event.target).closest('.' + this.selectorCls.selectorUl);
-            this._trigger('remove', event, ui);
-        },
-        /**
-         * add item
-         * @returns {}
-         */
-        _addItem: function(value, text, ct) {
-            var mutable = this.options.mutable,
-                    liData = {
-                text: text,
-                removeBtn: mutable ? '<button class="' + this.selectorCls.removeCls + '">' + this.options.removeText + '</button>' : ''
-            };
-            if(!ct){
-                ct = this.uiSelector;
+            var target = $(event.target),
+                targetLi = target.closest('li.' + this.selectorCls.liCls),
+                level = targetLi.attr('level'),
+                selectorData = targetLi.data('selector-data'),
+                ui = $(event.target).closest('.' + this.selectorCls.ulCls);
+            targetLi.remove();
+            if(ui.children('li').length === 0){
+                ui.parent().remove();
             }
-            var liEl = $(this.selectorLi).appendTo(ct).addClass(this.selectorCls.selectableCls + ' ui-corner-all');
-            $(applyTemplate(this.selectorContent, liData)).appendTo(liEl).data('selector-value', value);
+            this._trigger('remove', selectorData,level);
         },
         
-        _addItemCt: function(value,text, ct){
-            if(!ct){
-                ct = this.uiSelector;
-            }
-            var liEl = $(this.selectorLi).appendTo(ct).addClass(this.selectorCls.labelCls + ' ui-corner-all');
-            $(applyTemplate(this.selectorContent, {text: text})).appendTo(liEl).data('selector-value', value);
+        _addItem: function(config) {
+            var mutable = this.options.mutable,
+                liEl;
             
-            liEl = $(this.selectorLi).appendTo(ct);
-            var ulEl = $(this.selectorUl).appendTo(liEl);
-            return ulEl;
+            var id = config.id,
+                value = config.value,
+                text = config.text,
+                leaf = config.leaf,
+                level = config.level,
+                insert = config.insert,
+                ct = config.ct || this.uiSelector;
+        
+            liEl = $(this.selectorLi)[insert == 1 ? 'insertAfter' : 'appendTo'](ct).addClass(this.selectorCls.selectableCls + ' ui-corner-all').attr('level', level).attr('id', id)
+                    .data('selector-data', {id: id, value: value, text: text});
+            if (leaf) {
+                liData = {
+                    text: text,
+                    removeBtn: mutable ? '<button class="' + this.selectorCls.removeCls + '">' + this.options.removeText + '</button>' : ''
+                };
+                liEl.addClass('selector-data');
+                $(applyTemplate(this.selectorContent, liData)).appendTo(liEl);
+            }else{//如果不是叶子节点，需要添加父元素
+                $(this.header).appendTo(liEl).text(text);
+                var ulEl = $(this.selectorUl).appendTo(liEl);
+                return ulEl;
+            }
+        },
+        
+        /**
+         * 通过传入的数据动态生成selector tree元素
+         * @param {Array} data 数据
+         * @param {Number} level 层次
+         * @param {jQuery} ct 容器
+         * @returns {}
+         */
+        _createItem: function(data, level, ct){
+            if(!data){
+                return;
+            }
+            if(!$.isArray(data)){
+                data = [data];
+            }
+            var leaf, cloneData;
+            for(var i = 0 ,len = data.length; i<len; i++){
+                leaf = (data[i].children && data[i].children.length  > 0) ? false : true;
+                cloneData = $.extend({}, data[i], {ct: ct, level : level,leaf : leaf});
+                var ulEl = this._addItem(cloneData);
+                if(leaf === false && ulEl){
+                     this._createItem(data[i].children, level + 1, ulEl);
+                }
+            }
+        },
+        
+        _insertItem: function(data, level, sibling){
+            if(!data){
+                return;
+            }
+            if(!$.isArray(data)){
+                data = [data];
+            }
+            var leaf, cloneData,
+                len = data.length;
+            for(var i = len - 1; i >= 0; i--){
+                leaf = (data[i].children && data[i].children.length  > 0) ? false : true;
+                cloneData = $.extend({}, data[i], {ct: sibling, level : level,leaf : leaf,insert:1});
+                var ulEl = this._addItem(cloneData);
+                if(leaf === false && ulEl){
+                     this._createItem(data[i].children, level + 1, ulEl);
+                }
+            }
         },
                 
-        add: function(value, text) {
-            this._addItem(value, text);
-            this._trigger('remove', this.uiSelector);
+        add: function(data, clear) {
+            if(clear === true){
+                this.clear();
+            }
+            this._createItem(data, 0);
         },
+         
+        insert: function(data, position) {
+            if (typeof position == 'string') {
+                position = $('#' + position);
+            } else {
+                position = $(position);
+            }
+            if(position.length != 0){
+                this._insertItem(data, position.attr('level'), position);
+            }
+        },
+                
+        /**
+         * 清空所有数据
+         * @returns {}
+         */
+        clear : function(){
+            this.uiSelector.empty();
+        },
+                
         select: function(event) {
 
+        },
+                
+        getData : function(selected){
+            var data = [], s = 'li.selector-data';
+            if(selected){
+               s += ' ui-state-active'; 
+            }
+            this.uiSelector.find(s).each(function(){
+                var selectorData = $(this).data('selector-data');
+                data.push(selectorData);
+            });
+            return data;
         }
 
     });
